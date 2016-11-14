9f53e666421dbbf42ba178ae26bf3836c10cc336
Lukas Muller <lamueller@protonmail.com>
2016-11-14 12:08:55 -0800

    Update encipher of Dart

--- src/dart/decipher.dart
+++ src/dart/decipher.dart
@@ -1,99 +1,89 @@
-String back_translation(String number) {
-  switch (number) {
-    case "1":
-  		return "A";
-  	case "2":
-  		return "B";
-  	case "3":
-  		return "C";
-  	case "4":
-  		return "D";
-  	case "5":
-  		return "E";
-  	case "6":
-  		return "F";
-  	case "7":
-  		return "G";
-  	case "8":
-  		return "H";
-  	case "9":
-  		return "I";
-  	case "10":
-  		return "J";
-  	case "11":
-  		return "K";
-  	case "12":
-  		return "L";
-  	case "13":
-  		return "M";
-  	case "14":
-  		return "N";
-  	case "15":
-  		return "O";
-  	case "16":
-  		return "P";
-  	case "17":
-  		return "Q";
-  	case "18":
-  		return "R";
-  	case "19":
-  		return "S";
-  	case "20":
-  		return "T";
-  	case "21":
-  		return "U";
-  	case "22":
-  		return "V";
-  	case "23":
-  		return "W";
-  	case "24":
-  		return "X";
-  	case "25":
-  		return "Y";
-  	case "26":
-  		return "Z";
-  	case "27":
-  		return "Ä";
-  	case "28":
-  		return "Ö";
-  	case "29":
-  		return "Ü";
-  	default:
-  		return "?";
+String letter_to_number(String letter) {
+  String uppercaseLetter = letter.toUpperCase();
+
+  switch (uppercaseLetter) {
+    case "A":
+      return "1";
+    case "B":
+      return "2";
+    case "C":
+      return "3";
+    case "D":
+      return "4";
+    case "E":
+      return "5";
+    case "F":
+      return "6";
+    case "G":
+      return "7";
+    case "H":
+      return "8";
+    case "I":
+      return "9";
+    case "J":
+      return "10";
+    case "K":
+      return "11";
+    case "L":
+      return "12";
+    case "M":
+      return "13";
+    case "N":
+      return "14";
+    case "O":
+      return "15";
+    case "P":
+      return "16";
+    case "Q":
+      return "17";
+    case "R":
+      return "18";
+    case "S":
+      return "19";
+    case "T":
+      return "20";
+    case "U":
+      return "21";
+    case "V":
+      return "22";
+    case "W":
+      return "23";
+    case "X":
+      return "24";
+    case "Y":
+      return "25";
+    case "Z":
+      return "26";
+    case "Ä":
+      return "27";
+    case "Ö":
+      return "28";
+    case "Ü":
+      return "29";
+    default:
+      return "0";
   }
 }
 
-String decipher(String encrypted_word) {
+String encipher(String word) {
+  var current_word = word.toUpperCase();
+  int word_length = current_word.length;
+  var split_word = current_word.split('');
 
-  bool ignore = false;
-  var chars = encrypted_word.split('');
-  int char_amount = chars.length;
-  String result = '';
+  String new_word = '';
 
-  for (var i = 0; i < char_amount; i++) {
-    if (ignore == false) {
-      if (chars[i] != ' ' && chars [i] != "-") {
-        var new_chars = chars[i];
-        var ni = i + 1; // Next index
-        if (ni != char_amount) {
-          if (chars[ni] != ' ' && chars[ni] != "-") {
-            new_chars += chars[ni];
-            ignore = true;
-          }
-        }
-        result += back_translation(new_chars);
-      } else if (chars[i] == ' ') {
-        result += ' ';
+  for (var i = 0; i < word_length; i++) {
+    if (split_word[i] != ' ') {
+      String current_letter = split_word[i];
+      new_word += letter_to_number(current_letter);
+
+      var next_i = i + 1;
+      if (i != (word.length - 1) && split_word[next_i] != ' ') {
+        new_word += "-";
       }
     } else {
-      ignore = false;
+      new_word += " ";
     }
   }
-  print(result);
-  return result;
-}
-
-
-main() {
-  decipher("8-5-12-12-15");
+  return new_word;
 }
